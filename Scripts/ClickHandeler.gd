d5ac7d8177aa42a85539c60ce4b8458af83c013c
Nathan Nieuwenhuizen <nathan-san@live.nl>
2020-07-11 16:23:50 +0200

    click event with charge

---
+++ Scripts/ClickHandeler.gd
@@ -0,0 +1,35 @@
+extends Node
+
+var power: float = 0
+export var clickTime: float = 0.5
+
+signal beatPressedSignal
+signal beatReleaseSignal(value)
+var clicked = false
+
+func _process(delta):
+	if (Input.is_action_pressed("beatClick")):
+		holdClick()
+	
+	if (Input.is_action_just_released("beatClick")):
+		releaseClick()
+	
+	if (clicked):
+		power += delta
+		if (power > clickTime):
+			releaseClick()
+
+func holdClick():
+	if (clicked):
+		return
+	print("pressed...")
+	emit_signal("beatPressedSignal")
+	clicked = true
+
+func releaseClick():
+	if (!clicked):
+		return
+	print("released... ")
+	emit_signal("beatReleaseSignal", power / clickTime)
+	power = 0
+	clicked = false
